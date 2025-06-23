02daefc2f1b8ff106bb115e404d81999279ba336
AndreaCatapano <a.catapano96@gmail.com>
2025-06-23 18:51:56 +0200

    feat: added new console log for better answers

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -13,21 +13,32 @@ public class PasswordGenerator {
 
         Scanner input = new Scanner(System.in);
 
+        System.out.print("Inserisci il tuo nome: ");
         name = input.nextLine();
         System.out.println("Il tuo nome è: " + name);
+
+        System.out.print("Inserisci il tuo cognome: ");
         surname = input.nextLine();
         System.out.println("Il tuo cognome è: " + surname);
+
+        System.out.print("Inserisci il tuo colore preferito: ");
         color = input.nextLine();
         System.out.println("Il tuo colore preferito è: " + color);
+
+        System.out.print("Inserisci il tuo anno di nascita: ");
         year = input.nextInt();
         System.out.println("Il tuo anno di nascita è: " + year);
+
+        System.out.print("Inserisci il tuo mese di nascita: ");
         month = input.nextInt();
         System.out.println("Il tuo mese di nascita è: " + month);
+
+        System.out.print("Inserisci il tuo giorno di nascita: ");
         day = input.nextInt();
         System.out.println("Il tuo giorno di nascita è: " + day);
 
         String password = name + "-" + surname + "-" + color + "-" + year;
-        System.out.println("La tua password è:" + password);
+        System.out.println("La tua password è: " + password);
 
     }
 }
